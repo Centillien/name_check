b60adcd157757f96380598eb1ab1941f4533abc3
Gerard Kanters <gkanters@gmail.com>
2014-08-20 15:14:50 +0200

    Update name_check_js.php

--- views/default/js/name_check_js.php
+++ views/default/js/name_check_js.php
@@ -11,13 +11,15 @@
 elgg.provide('elgg.name_check');
 
 $('input[name=name]').bind('input propertychange', function () {
-	var blacklist = /[\'/\\"*$#!%^(){}~?<>;|Â¬`@+=]/;
-	while (blacklist.test($(this).val())) {
-		$(this).val(function (i, v) {
-			return v.replace(blacklist, '');
-		}).val();
-	elgg.register_error(elgg.echo("name_check:invalidchar"));
-	}
+    var blacklist = /[\'/\\"*$#!%^(){}~?<>;|Â¬`@+=]/;
+    if (blacklist.test($(this).val())) {
+        elgg.register_error(elgg.echo("name_check:invalidchar", [blacklist]));
+    }
+    while (blacklist.test($(this).val())) {
+        $(this).val(function (i, v) {
+            return v.replace(blacklist, '');
+        }).val();
+    }
 });
 
 elgg_load_js('elgg.name_check');
